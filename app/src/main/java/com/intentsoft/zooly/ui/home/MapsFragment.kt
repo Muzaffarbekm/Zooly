945d90323cc08303da979f1844e356fd64b1dc60
sadulla <ubaydullayevsadulla843@gmail.com>
2021-10-31 10:46:58 +0500

    added home

--- app/src/main/java/com/intentsoft/zooly/ui/home/MapsFragment.kt
+++ app/src/main/java/com/intentsoft/zooly/ui/home/MapsFragment.kt
@@ -9,12 +9,17 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.*
 import com.intentsoft.zooly.R
 
-class MapsFragment : Fragment() {
+class MapsFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnPolylineClickListener {
+
+    val TamWorth: LatLng = LatLng(-31.083332, 150.916672)
+    val NewCastle: LatLng = LatLng(-31.083332, 150.916672)
+    val Brisbane: LatLng = LatLng(-31.083332, 150.916672)
 
     private val callback = OnMapReadyCallback { googleMap ->
 
@@ -23,9 +28,10 @@ class MapsFragment : Fragment() {
             BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)))
         googleMap.moveCamera(CameraUpdateFactory.newLatLng(Abay))
 
-        val Astana = LatLng(51.10, 71.30)
-        googleMap.addMarker(MarkerOptions().title("Astana").position(Astana).snippet("Astana").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)))
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(Astana))
+        googleMap.addPolyline((PolylineOptions()).add(Brisbane, NewCastle, TamWorth, Brisbane)
+            .width(10F)
+            .color(Color.RED)
+            .geodesic(true))
 
     }
 
@@ -43,5 +49,25 @@ class MapsFragment : Fragment() {
         mapFragment?.getMapAsync(callback)
     }
 
+    override fun onMapReady(googleMap: GoogleMap) {
 
+        // Add polylines to the map.
+        // Polylines are useful to show a route or some other connection between points.
+        val polyline1 = googleMap.addPolyline(PolylineOptions()
+            .clickable(true)
+            .add(
+                LatLng(-35.016, 143.321),
+                LatLng(-34.747, 145.592),
+                LatLng(-34.364, 147.891),
+                LatLng(-33.501, 150.217),
+                LatLng(-32.306, 149.248),
+                LatLng(-32.491, 147.309)))
+
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(LatLng(-23.684, 133.903), 4f))
+
+    }
+
+    override fun onPolylineClick(p0: Polyline) {
+
+    }
 }
